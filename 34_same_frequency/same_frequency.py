2c34676d8cc851e16d68a560adb7077fa2375652
Tammy <tlpainter3@gmail.com>
2023-11-30 17:08:03 -0600

    correction

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -1,17 +1,3 @@
-def same_frequency(num1, num2):
-    """Do these nums have same frequencies of digits?
-    
-        >>> same_frequency(551122, 221515)
-        True
-        
-        >>> same_frequency(321142, 3212215)
-        False
-        
-        >>> same_frequency(1212, 2211)
-        True
-    """
-
-
 def freq_counter(coll):
     """Returns frequency counter mapping of coll."""
 
